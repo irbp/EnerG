06139793c96e0f3cf24066c5fb05d9b82638f541
Goncamateus <mgm4@cin.ufpe.br>
2016-11-13 16:20:12 -0300

    first commit

--- app/src/main/java/goncinha/energ/Login.java
+++ app/src/main/java/goncinha/energ/Login.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
